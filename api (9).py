ce1233bfba2f2995530e4c5663e3b5e2c57add53
kobichen222 <kobi@media-deal.co.il>
2025-05-09 09:01:21 +0300

    Update api (9).py

--- api (9).py
+++ api (9).py
@@ -71,3 +71,10 @@ if __name__ == '__main__':
 
 port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0', port=port)
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
